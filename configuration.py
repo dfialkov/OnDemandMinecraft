f6d9a7be9e72964bb7d437363713b7bcd2723584
daniel <daniel@holographic.com>
2021-07-24 09:55:17 -0600

    Added configuration and changed python runtime

--- configuration.py
+++ configuration.py
@@ -1,17 +1,18 @@
 class Config:
-    #AWS Information
-    ACCESS_KEY = ''
-    SECRET_KEY = ''
-    INSTANCE_ID = ''
-    ec2_region = ""
-    ec2_amis = ['']
-    ec2_keypair = ''
-    ec2_secgroups = ['']
-    ec2_instancetype = ''
+    # AWS Information
+    ACCESS_KEY = 'AKIARNGELKMI4UHIMQ7T'
+    SECRET_KEY = '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'
+    INSTANCE_ID = 'i-0b61582c5b01aa051'
+    ec2_region = "us-west-2"
+    ec2_amis = ['ami-01773ce53581acf22']
+    ec2_keypair = 'MinecraftServerKeypair'
+    ec2_secgroups = ['MinecraftServer']
+    ec2_instancetype = 't3.large'
 
-    #SSH Key Path
-    SSH_KEY_FILE_PATH = ''
+    # SSH Key Path
+    SSH_KEY_FILE_PATH = './MinecraftServerKeypair.pem'
 
-    #Server Memory Size
-    #This is default to no memory specification but can be: '-Xmx1024M -Xms1024M ' (KEEP TRAILING SPACE)
-    MEMORY_ALLOCATION='' 
+    SERVER_PASSWORD = '39g4w8he9o8r5hyj'
+    # Server Memory Size
+    # This is default to no memory specification but can be: '-Xmx1024M -Xms1024M ' (KEEP TRAILING SPACE)
+    MEMORY_ALLOCATION = ''
